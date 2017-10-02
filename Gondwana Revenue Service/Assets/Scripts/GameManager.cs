a64da1ee2fc08e46ac79ff994267ec8c24964d74
gglyptodon <gglyptodon@gmail.com>
2017-10-02 22:44:59 +0200

    now with level transitons and text display

--- Gondwana Revenue Service/Assets/Scripts/GameManager.cs
+++ Gondwana Revenue Service/Assets/Scripts/GameManager.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
+    public float levelStartDelay = 10f;	
 	public float turnDelay = .1f;
 	public static GameManager instance = null;
 	public BoardManager boardScript;
@@ -10,9 +12,20 @@ public class GameManager : MonoBehaviour {
     public int playerSanity = 100;
 	[HideInInspector] public bool playersTurn = true;
 
-	private int level = 3;
+	private int level = 1;
 	private List<Enemy> enemies = new List<Enemy>(); // had to init as i didnt put anything via gui
 	private bool enemiesMoving;
+
+
+	// Text stuff
+
+	private Text taxErrorText; // text for level transition
+	private Text lesothosaurusText; // only used at the start
+
+
+	private GameObject taxErrorImage; // image for level transition
+
+	private bool doingSetup = true;
 
 	// Use this for initialization
 	void Awake () {
@@ -26,11 +39,17 @@ public class GameManager : MonoBehaviour {
 		InitGame ();
 	}
 
+	void OnLevelWasLoaded(int index){
+		level++;
+		InitGame ();
+	
+	}
+
        //Update is called every frame.
         void Update()
         {
             //Check that playersTurn or enemiesMoving or doingSetup are not currently true.
-            if(playersTurn || enemiesMoving)
+		if(playersTurn || enemiesMoving || doingSetup)
                 
                 //If any of these are true, return and do not start MoveEnemies.
                 return;
@@ -47,12 +66,39 @@ public class GameManager : MonoBehaviour {
         }
 	
 	public void GameOver (){
+		taxErrorText.text = "gameover";
+		taxErrorImage.SetActive (true);
 		enabled = false;
 	}
 	// Update is called once per frame
 	void InitGame () {
-		enemies.Clear ();
+		doingSetup = true;
+		taxErrorImage = GameObject.Find ("TaxErrorImage"); // aka levelImage
+		lesothosaurusText = GameObject.Find("LesothosaurusText").GetComponent<Text>();
+		taxErrorText = GameObject.Find("TaxErrorText").GetComponent<Text>(); // aka LevelText
+		if (level == 1) {
+			taxErrorText.text = "It's Tax Day... ";
+			lesothosaurusText.text = "...For Lesothosaurus";
+
+		} else {
+			taxErrorText.color = Color.red;
+			taxErrorText.text = "TAX ERRORO todo";
+
+			lesothosaurusText.text = "";
+		
+		}
+		taxErrorImage.SetActive (true); //display img
+		Invoke("HideTaxErrorImage", levelStartDelay); 
+
+		enemies.Clear (); // todo remove unneccessary things from the tutorial
 		boardScript.SetupScene (level);
+		
+	}
+
+	void HideTaxErrorImage(){
+		//Disable img
+		taxErrorImage.SetActive(false);
+		doingSetup = false;
 		
 	}
 
